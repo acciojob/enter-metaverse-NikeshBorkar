3880233c1fad0cc6bd77cc5cf650790b04a94b38
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-06 19:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn=document.getElementById("enterBtn")
+let change=document.getElementById("status")
+
+btn.addEventListener("click", (event) => {
+  change.innerHTML=`<h1>Entered Metaverse</h1>`
+});
